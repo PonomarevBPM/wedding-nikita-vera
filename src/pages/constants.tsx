aec38cc1b4463fced675099dfeced5af6caa1d47
PonomarevBPM <ponomarev.bpm@gmail.com>
2026-02-17 15:49:48 +0300

    New Timer date

--- src/pages/constants.tsx
+++ src/pages/constants.tsx
@@ -1,42 +1,42 @@
 export const header = {
-  date: '20.08.24',
-  names: 'DENIS & ANNA',
-  fistInitial: 'D',
-  secondInitial: 'A',
+    date: '20.08.27',
+    names: 'DENIS & ANNA',
+    fistInitial: 'D',
+    secondInitial: 'A'
 };
 
 export function NamedStatementText() {
-  return (
-    <>
-      Один день в этом году будет для нас
-      <br />
-      особенным и мы хотим провести его
-      <br />
-      в кругу близких и друзей.
-      <br />
-      С большим удовольствием приглашаем
-      <br />
-      вас на нашу свадьбу!
-    </>
-  );
+    return (
+        <>
+            Один день в этом году будет для нас
+            <br />
+            особенным и мы хотим провести его
+            <br />
+            в кругу близких и друзей.
+            <br />
+            С большим удовольствием приглашаем
+            <br />
+            вас на нашу свадьбу!
+        </>
+    );
 }
 
 // Timer deadline
-export const deadline = '20 Aug 2025 12:00:00 GMT';
+export const deadline = '20 Aug 2027 12:00:00 GMT';
 
 export function LocationText() {
-  return (
-    <>
-      The Birch
-      <br />
-      Зал BLACKWOOD HOUSE
-      <br />
-      <br />
-      Московская область,
-      <br />
-      городской округ Солнечногорск,
-      <br />
-      территориальное управление Пешковское
-    </>
-  );
+    return (
+        <>
+            The Birch
+            <br />
+            Зал BLACKWOOD HOUSE
+            <br />
+            <br />
+            Московская область,
+            <br />
+            городской округ Солнечногорск,
+            <br />
+            территориальное управление Пешковское
+        </>
+    );
 }
